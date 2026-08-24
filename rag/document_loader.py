392bba42cb77c07b5e04110a60dfc0ad92b53a8e
shauryajain <shaurya9@uw.edu>
2026-08-25 01:10:43 +0530

    Loading in documents

--- rag/document_loader.py
+++ rag/document_loader.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from pypdf import PdfReader
+import pandas as pd
 
 
 class DocumentLoader:
@@ -8,9 +10,73 @@ class DocumentLoader:
     def __init__(self, filepath):
         self.filepath = Path(filepath)
 
-    def load(self):
+    def load(self, filepath):
         """
         Loading filepath as document
         """
-        doc = self.filepath.read_text(encoding='utf-8')
+        doc_type = filepath[-4:]
+        if doc_type == ".pdf":
+            self.load_pdf(filepath)
+        elif doc_type == ".txt":
+            self.load_txt(filepath)
+        elif doc_type == ".csv":
+            self.load_csv(filepath)
+        else:
+            raise ValueError(f"Document type {doc_type} is unknown")
+
+        # doc = self.filepath.read_text(encoding='utf-8')
+        # return doc
+
+    def load_pdf(self, filepath):
+        """
+        If the document uploaded is a pdf, it reads and stores that
+        """
+        documents = []
+        pdf = PdfReader.read(filepath)
+        for page in range(len(pdf.pages)):
+            text = page.extract_text()
+            doc = Document(metadata={"source": filepath,
+                                     "page": page},
+                           text=text)
+            documents.append(doc)
+        return documents
+
+    def load_txt(self, filepath):
+        """
+        If the document uploaded is a text file, it reads and stores that
+        """
+        text = None
+        with open(filepath) as f:
+            text = f.read()
+
+        doc = Document(metadata={"source": filepath,
+                                 "page": None}, text=text)
+        return [doc]
+
+    def load_csv(self, filepath):
+        """
+        If the document uploaded is a csv, it reads and stores that
+        """
+        csv = pd.read_csv(filepath)
+        txt = csv.to_csv('output.txt', sep='\t', index=False)
+        doc = Document(metadata={"source": filepath,
+                                 "type": "dataset"},
+                       text=txt)
         return doc
+
+    # def load_markdown(self, filepath):
+    #     """
+    #     If the document uploaded is a markdown, it reads and stores that
+    #     """
+
+
+class Document:
+    """
+    This class is information about the actual document
+    """
+    def __init__(self, metadata, text):
+        self.metadata = metadata
+        self.text = text
+
+# text -> whatever is in the document
+# metadata -> dictionary of source and page
